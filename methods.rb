031e93b251e424365309242287455972d4cc13c1
Yazeed <dee3ayahmed25@gmail.com>
2023-05-19 16:37:00 +0300

    completed

--- methods.rb
+++ methods.rb
@@ -1,1 +1,25 @@
-# Your code here!
+def greet_programmer
+    puts "Hello, programmer!"
+end
+
+def greet (name = "Ys")
+    puts "Hello, #{name}!"
+end
+
+def greet_with_default (name = "programmer")
+    puts "Hello, #{name}!"
+end
+
+def add(number1,number2)
+    return number1 + number2
+end
+
+def halve number
+    if number.class != Integer
+         pp nil
+    else
+    pp number / 2
+    end
+end
+
+
